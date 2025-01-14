47721625bdd1b4e75aee3a66d2e8955588a688bb
Priyanshu Saini <85929114+priyanshu-sai@users.noreply.github.com>
2025-01-14 11:30:00 +0000

    testing

---
+++ basics/dataTypes.js
@@ -0,0 +1,15 @@
+/*
+"use strict" treat all js code newer version
+*/
+
+// number = 2 to power 53 
+// bigInt
+// string = "" 
+// boolen = true,false 
+// null 
+//undefined
+// object 
+//symbol
+
+// we can say type of null [object]
+console.log(typeof(string));
---
+++ basics/variable.js
@@ -0,0 +1,8 @@
+const accountId = 123444;
+let name = 'test';
+var age = 18;
+let abc = 'abc';
+err = 'test1323'
+
+console.log(abc);
+console.table([accountId,name,age,abc,err]);
